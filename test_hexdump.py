92afe4fee0502c0d5ac1ecdb1036a7a7a81db661
lbreede <67719520+lbreede@users.noreply.github.com>
2024-09-14 16:19:05 -0700

    Refactor hexdump test function for improved performance and readability

--- test_hexdump.py
+++ test_hexdump.py
@@ -1,69 +1,94 @@
+import argparse
+import logging
 import os
 import subprocess
-import hexdump
-import logging
+import time
 
+from tqdm import trange
+
+import hexdump
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
-FILENAME = "hello.txt"
+FILENAME = "lorem.txt"
 TEST_DIR = "test"
 
 
-def test_hexdump():
-    with open(FILENAME, "r", encoding="utf-8") as fp:
+def test_hexdump(min_cols: int = 1, max_cols: int = 256) -> None:
+    with open(FILENAME, "rb") as fp:
         data = fp.read()
 
-    max_cols = 256
-    logger.info("Testing hexdump with maximum of %i", max_cols)
+    logger.info("🧪 Testing hexdump from %i to %i", min_cols, max_cols)
     results = []
-    for cols in range(1, max_cols + 1):
+    start = time.time()
+    for cols in trange(min_cols, max_cols + 1):
         for groupsize in range(1, cols + 1):
-            _hexdump(data, cols, groupsize, False)
-            results.append(_compare_files())
-            _cleanup()
-            _hexdump(data, cols, groupsize, True)
-            results.append(_compare_files())
-            _cleanup()
+            results.append(test_single_hexdump(data, cols, groupsize, False))
+            results.append(test_single_hexdump(data, cols, groupsize, True))
+    logger.info("Time taken: %.2f seconds", time.time() - start)
     if all(results):
-        logger.info("All tests passed")
+        logger.info("🎉 All tests passed")
+        os.rmdir(TEST_DIR)
+    else:
+        logger.error("❌ Some tests failed")
 
 
-def _hexdump(data: str, cols: int, groupsize: bytes, uppercase: bool) -> str:
-    output = hexdump.hexdump(data, cols, groupsize, uppercase)
-    if not os.path.isdir(TEST_DIR):
-        os.makedirs(TEST_DIR)
-    with open(f"{TEST_DIR}/pyxd.txt", "w", encoding="utf-8") as fp:
-        fp.write(output)
+def run_single_hexdump(data: bytes, cols: int, groupsize: int, uppercase: bool) -> str:
+    case = "upper" if uppercase else "lower"
+    file = f"{TEST_DIR}/pyxd-{cols}-{groupsize}-{case}.txt"
+    with open(file, "w", encoding="utf-8") as fp:
+        hexdump.hexdump(data, cols, groupsize, uppercase, fp=fp)
+    return file
 
+
+def run_single_xxd(cols: int, groupsize: int, uppercase: bool) -> str:
+    case = "upper" if uppercase else "lower"
     args = ["xxd", "-c", str(cols), "-g", str(groupsize)]
     if uppercase:
         args.append("-u")
     args.append(FILENAME)
-
-    with open(f"{TEST_DIR}/xxd.txt", "w", encoding="utf-8") as fp:
+    file = f"{TEST_DIR}/xxd-{cols}-{groupsize}-{case}.txt"
+    with open(file, "w", encoding="utf-8") as fp:
         subprocess.run(args, check=True, stdout=fp)
+    return file
 
 
-def _compare_files() -> bool:
-    with open("test/pyxd.txt", "r", encoding="utf-8") as fp1:
-        with open("test/xxd.txt", "r", encoding="utf-8") as fp2:
+def test_single_hexdump(
+    data: bytes, cols: int, groupsize: int, uppercase: bool
+) -> bool:
+    if not os.path.isdir(TEST_DIR):
+        os.makedirs(TEST_DIR)
+
+    file_a = run_single_hexdump(data, cols, groupsize, uppercase)
+    file_b = run_single_xxd(cols, groupsize, uppercase)
+
+    return _compare_files(file_a, file_b)
+
+
+def _compare_files(file_a: str, file_b: str) -> bool:
+    with open(file_a, "r", encoding="utf-8") as fp1:
+        with open(file_b, "r", encoding="utf-8") as fp2:
             for line1, line2 in zip(fp1, fp2):
                 if line1 != line2:
+                    logger.error("Mismatch between %r and %r", file_a, file_b)
+                    logger.error("File A: %r", line1.strip())
+                    logger.error("File B: %r", line2.strip())
                     return False
+    os.remove(file_a)
+    os.remove(file_b)
     return True
 
 
-def _cleanup() -> None:
-    if os.path.isdir(TEST_DIR):
-        for file in os.listdir(TEST_DIR):
-            os.remove(os.path.join(TEST_DIR, file))
-        os.rmdir(TEST_DIR)
-
-
 def main() -> None:
-    test_hexdump()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n", "--min-cols", type=int, default=1, help="Minimum number of columns"
+    )
+    parser.add_argument(
+        "-x", "--max-cols", type=int, default=256, help="Maximum number of columns"
+    )
+    test_hexdump(**vars(parser.parse_args()))
 
 
 if __name__ == "__main__":
